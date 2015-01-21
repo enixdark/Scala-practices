2ebf519f1520e52c010ee0bcf3bf00ed573059fc
enixdark <cqshinn92@gmail.com>
2015-01-21 20:25:35 +0700

    simple play 2

---
+++ play-scala/app/controllers/Items.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.mvc._
+
+// object Items extends Controller {
+// 	val shop = models.Shop
+// 	val list = Action
+// 	val create = Action
+// 	def update(id: Long) = Action
+// 	def remove(id: Long) = Action
+// }
